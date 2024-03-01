f3828b8d4aaf6024e81f74d85923a145cea6fb68
YoungBear <youngbear@aliyun.com>
2024-03-01 21:54:50 +0800

    [add] add test api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping(value = "/demo/current", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> current() {
+        ZonedDateTime now = ZonedDateTime.now();
+        Map<String, String> result = new HashMap<>();
+        result.put("zonedDateTime", now.toString());
+        result.put("timestamp", String.valueOf(now.toInstant().toEpochMilli()));
+        LOGGER.info("zonedDateTime: {}", now);
+        LOGGER.info("timestamp: {}", now.toInstant().toEpochMilli());
+        return result;
     }
 
 }
